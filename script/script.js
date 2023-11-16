60e6f16ee2780b0111f40a744ac209b65efef8f7
cnorwood10 <142539217+cnorwood10@users.noreply.github.com>
2023-11-16 14:19:30 -0600

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,35 @@
+
+document.getElementById('dropdown').addEventListener('change', function() {
+    var value = this.value;
+
+    // Clear the previous content
+    document.getElementById('content').innerHTML = '';
+
+    // Load the new content based on the selected option
+    if (value === '1') {
+        document.getElementById('content').innerHTML = 'Content for Option 1'
+    } else if (value === '2') {
+        document.getElementById('content').innerHTML = 'Content for Option 2'
+    } else if (value === '3') {
+        document.getElementById('content').innerHTML = 'Content for Option 3'
+    } else if (value === '4') {
+        document.getElementById('content').innerHTML = 'Content for Option 4'
+    } else {
+        alert("Invalid selection")
+    }
+});
+
+
+$("input[type=drop][name=option]").change(function () {
+    //if they say no, display the textbox
+
+    //get value of the thing
+    let select = $("input[name=entertained]:checked").val();
+
+    //act accordingly
+    if (entertain === "No") {
+      $("#divExplain").show();
+    } else {
+      $("#divExplain").hide();
+    }
+  });
